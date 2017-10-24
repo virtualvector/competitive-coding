d1d65bf238feb9acc55b62e4ae07b4ff6f84254d
rohithrajreganti <rohithrajreganti@gmail.com>
2017-10-24 18:52:41 +0530

    buying sweets

---
+++ codechef/buying_sweets.cpp
@@ -0,0 +1,81 @@
+#pragma warning(disable:4786)
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<map>
+#include<functional>
+#include<string>
+#include<cstring>
+#include<cstdlib>
+#include<queue>
+#include<utility>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include<stack>
+#include<assert.h>
+using namespace std;
+
+
+
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+#define IN(A, B, C)  (B) <= (A) && (A) <= (C)
+
+
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+
+#define AIN(A, B, C) assert(IN(A, B, C))
+
+//typedef int LL;
+//typedef long long int LL;
+//typedef __int64 LL;
+
+int main()
+{
+
+ios_base::sync_with_stdio(0);
+
+    int test;
+    cin>>test;
+    while(test--){
+            
+            int notes,cost;
+            cin>>notes>>cost;
+            vector<int> v(notes);
+            for(int i=0;i<v.size();i++){
+                
+                cin>>v[i];
+
+
+            }
+            sort(v.begin(),v.end());
+            int total=0;
+            for(int i=0;i<v.size();i++)total+=v[i];
+            int number_of_sweets=total/cost;
+            int remaining = total-(number_of_sweets*cost);
+
+            if(v[0]<=remaining){
+                cout<<-1<<endl;
+
+            }
+            else cout<<number_of_sweets<<endl;
+
+    }
+
+
+return 0;
+}
+
