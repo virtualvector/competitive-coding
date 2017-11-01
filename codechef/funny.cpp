d521f26ae3db39179006915e24d6a09c675180a8
rohithrajreganti <rohithrajreganti@gmail.com>
2017-11-01 22:40:32 +0530

    funny.cpp

---
+++ codechef/funny.cpp
@@ -0,0 +1,74 @@
+#pragma warning(disable:4786)
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<map>
+#include<functional>
+#include<string>
+#include<cstring>
+#include<cstdlib>
+#include<queue>
+#include<utility>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include<stack>
+#include<assert.h>
+using namespace std;
+
+
+
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+#define IN(A, B, C)  (B) <= (A) && (A) <= (C)
+
+
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+
+#define AIN(A, B, C) assert(IN(A, B, C))
+
+//typedef int LL;
+//typedef long long int LL;
+//typedef __int64 LL;
+
+int main()
+{
+
+ios_base::sync_with_stdio(0);
+int n;
+cin>>n;
+vector<int> v(n);
+
+for(int i=0;i<n;i++){
+    cin>>v[i];
+}
+
+    int elem=0;
+    vector<int>::iterator iter;
+    for(iter=v.begin();iter!=v.end()-1;iter++){
+
+        int cal=find_end(v.begin(),v.end(),iter,iter+1)-iter;
+        if(cal>elem){
+            elem=cal;
+        }
+
+    }
+    cout<<elem+1<<endl;
+
+
+
+return 0;
+}
+
