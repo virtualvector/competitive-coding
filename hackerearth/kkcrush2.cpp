8f49e0a040c962e6ea02fabe07891cc9e9372e33
rohithrajreganti <rohithrajreganti@gmail.com>
2018-02-12 21:44:02 +0530

    second approach

---
+++ hackerearth/kkcrush2.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<unordered_set>
+#include<vector>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+
+   int test;
+   cin>>test;
+   while(test--)
+   {
+    int num;
+    cin>>num;
+    
+    vector<int> us;
+    while(num--)
+    {
+        int val;
+        cin>>val;
+        us.push_back(val);
+
+    }
+    sort(v.begin(),v.end());
+    int queries;
+    cin>>queries;
+    while(queries--)
+    {
+        
+        int q;
+        cin>>q;
+        if(binary_search(v.begin(),v.end(),q)
+        {
+            cout<<"Yes"<<endl;
+
+        }
+        else cout<<"No"<<endl;
+
+
+    }
+
+
+   }
+
+return 0;
+}
