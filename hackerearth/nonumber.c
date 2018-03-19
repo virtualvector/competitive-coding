28aa4b848d25173f2fa40f066b7be537e838806c
rohithrajreganti <rohithrajreganti@gmail.com>
2018-03-19 22:55:55 +0530

    nonumbr

---
+++ hackerearth/nonumber.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+ char ch = 'v';
+ counter++;
+ while(ch-'0'!=' ')
+ {
+    printf("%d\n",ch);
+    ch+=1;
+ }
+}
---
+++ hackerearth/nonumber.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    int counter=NULL;
+    char ch = 'F';
+
+    while(ch!='Q')
+    {
+        cout<<ch-'a'<<endl;
+        ch++;
+
+
+    }
+return 0;
+}
+
