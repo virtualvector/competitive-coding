9ee26141db24a3b6fa37831304baac7f54ee812a
rohithrajreganti <rohithrajreganti@gmail.com>
2018-03-20 19:12:21 +0530

    pivot

--- hackerearth/find_pivot.cpp
+++ hackerearth/find_pivot.cpp
@@ -1,32 +1,63 @@
 #include<iostream>
 #include<algorithm>
-#include<string>
+#include<cstdlib>
 #include<vector>
-#include<sstream>
+#include<string>
+#include<cstring>
+#include<cstdio>
 
 using namespace std;
 
 
 int main()
 {
-        vector<string> v;
+    int test;
+    cin>>test;
+    cin.ignore();
+
+    while(test--)
+    {
+
         string s;
-       getline(cin,s);
+        getline(cin,s,'\n');
+        vector<int> v;
+        char* tok = strtok(const_cast<char*>(s.c_str())," ");
 
-        stringstream ss(s);
-    string item;
-    vector<string> tokens;
-    while (getline(ss, item, ' ')) {
-        v.push_back(item);
-    }
- 
-
-        for(int i=0;i<v.size();i++)
+        while(tok!=NULL)
         {
-            cout<<s[i];
+                
+            v.push_back(atoi(tok));
+            tok = strtok(NULL," ");
 
         }
-        cout<<endl;
+           vector<int>::iterator iter;
+           bool flag = false;
+           for(iter=v.begin();iter!=v.end();iter++)
+           {
+                int left_sum = accumulate(v.begin(),iter,0);
+                vector<int>::iterator iter2;
+                iter2=iter;
+                int right_sum = accumulate(++iter2,v.end(),0);
+  //              cout<<"left sum is "<<left_sum<<" right sum is "<<right_sum<<endl;
+                if(left_sum==right_sum)
+                {
+                    cout<<(iter-v.begin())<<endl;
+                    flag = true;
+
+                }
+
+
+           }
+
+            if(flag==false) cout<<-1<<endl;
+
+
+//        for(int i=0;i<v.size();i++) cout<<v[i]<<" ";
+ //       cout<<endl;
+    }
+
+
+
 
 return 0;
 }
