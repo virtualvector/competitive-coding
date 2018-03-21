5aeea517bc6300628252dbbd5aadf1b0e5464d92
rohithrajreganti <rohithrajreganti@gmail.com>
2018-03-21 22:00:04 +0530

    few

---
+++ hackerearth/substr_question.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+
+int main()
+{
+
+    string a,b;
+    cin>>a;
+    cin>>b;
+
+    vector<int> v;
+    map<char,int>
+    for(int i=0;i<b.size();i++)
+    {
+
+
+    }
+
+    for(int i=0;i<b.size();i++)
+    {
+        auto pos = a.find_first_of(b[i]);
+        v.push_back(pos);
+
+
+    }
+
+    for(int i=0;i<v.size();i++)
+    {
+
+        cout<<v[i]<<" ";
+
+
+    }
+
+
+return 0;
+}
